78ab59bcc1575e8dce1c60353849a15b48e173b6
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2024-03-22 18:29:43 -0400

    add long description

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name="public_invest_api",
     version="1.0.1",
-    description="Unofficial Public.com API",
+    description="Unofficial Public.com Invest API",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/NelsonDane/public-invest-api",
     author="Nelson Dane",
     packages=["public_invest_api"],
