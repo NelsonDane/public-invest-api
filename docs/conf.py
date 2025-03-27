609555ec3d00bf327d629ded8253efa8115ab149
Nelson Dane <47427072+NelsonDane@users.noreply.github.com>
2025-03-27 19:29:35 -0400

    fix path

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../../public_invest_api"))
+sys.path.insert(0, os.path.abspath("../public_invest_api"))
 
 # Configuration file for the Sphinx documentation builder.
 #
